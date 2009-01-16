0e5b34c3fd0fc1d7743403fc857a8b67e0ff2485
corwin.j <corwin.j@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-16 00:12:13 +0000

    Adding glutFramework

---
+++ source/glutFramework/glutFramework/projekt1App.cpp
@@ -0,0 +1,17 @@
+#include "projekt1App.h"
+
+projekt1App::projekt1App(std::string title, int sizeX, int sizeY, int argc, char **argv)
+	: oglApp(title, sizeX, sizeY, argc, argv)
+{
+
+}
+
+void projekt1App::updateScene(float elapsed)
+{
+
+}
+
+void projekt1App::drawScene()
+{
+	oglApp::drawScene();
+}
