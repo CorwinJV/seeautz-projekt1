eb901e98ba46b2367de5039e6cd9831d7864892d
corwin.j <corwin.j@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-16 00:12:08 +0000

    Adding glutFramework

---
+++ source/glutFramework/glutFramework/program.cpp
@@ -0,0 +1,20 @@
+//#include "oglApp.h"
+#include "projekt1App.h"
+
+// App object
+oglApp* newApp = NULL;
+
+// Function prototypes
+void doRendering( void );
+
+int main(int argc, char **argv)
+{
+	newApp = new projekt1App("Awesome", 800, 600, argc, argv);
+	glutDisplayFunc(doRendering);
+	newApp->run();
+}
+
+void doRendering( void )
+{
+	newApp->drawScene();
+}
