b4d12bdf711cb2ca149f35c8bc49d29073cfa8f0
albatross4242@yahoo.com <albatross4242@yahoo.com@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-19 23:34:51 +0000

    Initial Version of GameState.cpp and GameState.h

--- seeautz/source/glutFramework/glutFramework/GameState.cpp
+++ seeautz/source/glutFramework/glutFramework/GameState.cpp
@@ -1,13 +1,70 @@
 #include "GameState.h"
 
-GameState::GameState()
+GameState::GameState()	// constructor
 {
+    //graphics = new GraphicsDeviceManager(this);
+    //content = new ContentManager(Services);
+    // graphics.IsFullScreen = true;
 
-
-
+    initialize();
 }
-
 GameState::~GameState()
 {
 
+}
+
+bool GameState::initialize()
+{
+	// debug options
+	displayCollisionDebug = true;
+
+
+    // Initialize status
+    currentStatus = Active;
+
+    // Initialize Spritebatch to render with
+    //batchRender = new SpriteBatch(graphics.GraphicsDevice);
+
+    // Initialize camera
+    //camera = new Camera2D(batchRender, graphics.GraphicsDevice);
+
+    // Initialize object manager
+    //myObjManager = new GameObjectManager();
+
+
+    // TODO: Create objects and add them to the object manager
+
+    // get screen size
+    //screenHeight = graphics.PreferredBackBufferHeight;
+    //screenWidth = graphics.PreferredBackBufferWidth;
+
+	return true;
+	// if something horribly bad happens return false
+}
+
+bool GameState::Update()//GameTime gameTime)
+{
+    // The only important line for managing objects
+   // myObjManager.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+    //myObjManager.CollectGarbage();
+
+	return true;
+	// if something horribly bad happens return false
+}
+
+bool GameState::Draw()//GameTime gameTime)
+{
+    //graphics.GraphicsDevice.Clear(Color.Black);
+
+    //batchRender.Begin();
+    /*foreach (BasicObject tempObject in myObjManager.ObjectList)
+    {
+        camera.DrawObject(tempObject);
+        if (displayCollisionDebug)
+            camera.DrawCollisionBoxes(tempObject);
+    }*/
+    //batchRender.End();
+
+	return true;
+	// if something horribly bad happens return false
 }
--- seeautz/source/glutFramework/glutFramework/GameState.h
+++ seeautz/source/glutFramework/glutFramework/GameState.h
@@ -1,14 +1,28 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 
+enum State { 
+				Active, // updating
+				Hidden, // Not drawing
+};
+
 class GameState
 {
+private:
+	// Current State
+	State currentStatus;
+
+	bool displayCollisionDebug;
+    int screenHeight;
+    int screenWidth;
+
 public:
 	GameState();
 	~GameState();
-
-private:
-
+	
+	bool initialize();
+	bool Update();
+    bool Draw();
 };
 
 #endif
