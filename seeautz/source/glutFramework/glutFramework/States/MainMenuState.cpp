118a29f78ef76d42bd2356a0bfc0c060cd5581c8
corwin.j <corwin.j@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-24 02:06:32 +0000

    Set up the MainMenuState to load up with a MenuSys object correctly. Working on Functors and callbacks next!

--- seeautz/source/glutFramework/glutFramework/States/MainMenuState.cpp
+++ seeautz/source/glutFramework/glutFramework/States/MainMenuState.cpp
@@ -2,10 +2,15 @@
 
 bool MainMenuState::Update()
 {
+	if(myMenu != NULL)
+		myMenu->Update();
+
 	return true;
 }
 
 bool MainMenuState::Draw()
 {
+	if(myMenu != NULL)
+		myMenu->Draw();
 	return false;
 }
