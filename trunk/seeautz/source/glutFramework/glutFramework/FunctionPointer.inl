6712459f6fbb4d1334e37d3ae6e6ab0880293ae7
corwin.j <corwin.j@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-26 23:21:01 +0000

    Added support for FunctionPointers to the MenuSys / Button system. In order to connect a Callback to a button, you must use the following constructor while attempting to add a button to the MenuSys.
    myMenu->addButton("button1normal.bmp", "button1down.bmp", "button1over.bmp", CreateFunctionPointer0R(this, &MainMenuState::button1Callback));
    
    The function defined inside of your class must be in the format of: bool button1Callback(void); where button1Callback can be whatever function name you want.

---
+++ trunk/seeautz/source/glutFramework/glutFramework/FunctionPointer.inl
@@ -0,0 +1,5 @@
+namespace BE
+{
+	
+
+} // namespace BE
--- trunk/seeautz/source/glutFramework/glutFramework/TestState.cpp
+++ trunk/seeautz/source/glutFramework/glutFramework/TestState.cpp
@@ -14,10 +14,10 @@ TestState::TestState(GameStateManager &Parent, int newID) : GameState(Parent, ne
 
 	myMenu = new MenuSys(250, 50, "blankmenu.bmp", Auto);
 	//myMenu = new MenuSys();
-	myMenu->addButton( 25, 100, "button1normal.bmp", "button1down.bmp", "button1over.bmp");
-	myMenu->addButton( 125, 200, "button2normal.bmp", "button2down.bmp", "button2over.bmp");
-	myMenu->addButton( 150, 300, "button3normal.bmp", "button3down.bmp", "button3over.bmp");
-	myMenu->addButton( 225, 400, "button4normal.bmp", "button4down.bmp", "button4over.bmp");
+	myMenu->addButton("button1normal.bmp", "button1down.bmp", "button1over.bmp");
+	myMenu->addButton("button2normal.bmp", "button2down.bmp", "button2over.bmp");
+	myMenu->addButton("button3normal.bmp", "button3down.bmp", "button3over.bmp");
+	myMenu->addButton("button4normal.bmp", "button4down.bmp", "button4over.bmp");
     initialize();
 }
 TestState::~TestState()
