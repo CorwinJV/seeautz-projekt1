ef9f13cc6809e10cf7bbd73047aa0a0d7618398b
albatross4242@yahoo.com <albatross4242@yahoo.com@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-27 22:41:11 +0000

    added drawtile function

--- seeautz/source/glutFramework/glutFramework/gameBoard.cpp
+++ seeautz/source/glutFramework/glutFramework/gameBoard.cpp
@@ -1,37 +1,3 @@
-#include "gameBoard.h"
-
-using namespace std;
-//class gameBoard()
-//{
-//protected:
-//	vector<vector<mapTile>> mapListX;
-//	// 1 dimensional array of ai entities
-//	int Width;
-//	int Height;
-//
-//public:
-		//	gameBoard(int nwidth, int nheight);
-//	~gameBoard();
-//  bool resizeBoard(int x, int y);
-		//	bool moveTile(int sX, int sY, int dX, int dY);
-//	bool update();
-//	bool draw();
-		//	bool setTileType(int x, int y, tileTypeEnum ntileType);
-		//	bool getTileType(int x, int y);	
-//	// bool addentity
-//
-//};
-
-//bool gameBoard::resizeBoard(int x, int y)
-//{
-//	mapList.resize(x);
-//	
-//	for(int i = 0; i < x; i++)
-//	{
-//		mapList[x].resize(i);
-//	}
-//}
-
 gameBoard::gameBoard(int nWidth, int nHeight)
 {
 	// make it all empty
@@ -87,12 +53,13 @@ bool gameBoard::draw()
 		vector<mapTile*>::iterator itr = mapList[x].begin();
 		for(int y = 0; y < Width; y++)
 		{
-			//drawTile((*itr)->getType(), x, y);
+			drawTile((*itr)->getType(), x, y);
 			itr++;
 		}
 	}
 	return true;
 }
+
 
 bool gameBoard::moveTile(int sX, int sY, int dX, int dY)
 {
@@ -109,4 +76,142 @@ bool gameBoard::setTileType(int x, int y, tileTypeEnum ntileType)
 tileTypeEnum gameBoard::getTileType(int x, int y)
 {
 	return mapList[x][y]->getType();
+}
+
+bool gameBoard::drawTile(tileTypeEnum nType, int txPos, int tyPos)
+{
+	pixmap* toDraw;
+
+	switch(nType)
+	{
+	case TEmpty:			// 0 empty
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TDefault:		// 1 default floor tile
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TRaised1:		// 2 raised 1 level
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TRaised2:		// 3 raised 2 levels
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TRaised3:		// 4 raised 3 levels
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TRaised4:		// 5 raised 4 levels
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case THalfTopL:		// 6 half top, left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case THalfTopR:		// 7 half top, right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case THalfBottomL:	// 8 half bottom, left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case THalfBottomR:	// 9 half bottom, right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TGap:			// 10 gap
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TElectric:		// 11 electric tile
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TElectricTL:	// 12 electric wall top left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TElectricTR:	// 13 electric wall top right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TElectricBL:	// 14 electric wall bottom left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TElectricBR:	// 15 electric wall bottom right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TIce:			// 16 ice tile
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TWater:			// 17 water tile
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TSwitchTL:		// 18 switch top left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TSwitchTR:		// 19 switch top right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TSwitchBL:		// 20 switch bottom left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TSwitchBR:		// 21 switch bottom right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TSwitch:		// 22 switch whole square
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TProgramTL:		// 23 reprogram spot top left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TProgramTR:		// 24 reprogram spot top right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TProgramBL:		// 25 reprogram spot bottom left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TProgramBR:		// 26 reprogram spot bottom right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TProgram:		// 27 reprogram spot
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TBreakableTL:	// 28 breakable top leftle
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TBreakableTR:	// 29 breakable top right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TBreakableBL:	// 30 breakable bottom left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TBreakableBR:	// 31 breakable bottom right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TSolid:			// 32 Solid Block
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TBreakable:		// 33 Solid Breakable Square
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TStart:			// 34 start square
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TEnd:			// 35 end square
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TDoorTL:		// 36 door top left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TDoorTR:		// 37 door top right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TDoorBL:		// 38 door bottom left
+		toDraw = NULL;		// set this to the proper image
+		break;
+	case TDoorBR:		// 39 door bottom right
+		toDraw = NULL;		// set this to the proper image
+		break;
+	default:
+		toDraw = NULL;		// set image to error image
+		break;
+	}
+
+	toDraw->mX = txPos;
+	toDraw->mY = tyPos;
+	toDraw->drawPixMap();
+
+	return true;
 }
