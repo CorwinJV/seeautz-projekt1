160d7e02dbdea74dbd9ceb02d381afb74c5659bb
tomlindeman@comcast.net <tomlindeman@comcast.net@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-28 23:27:11 +0000

    button 1 now brings you to a new state, and added new background colors to represent new states

--- seeautz/source/glutFramework/glutFramework/States/MainMenuState.h
+++ seeautz/source/glutFramework/glutFramework/States/MainMenuState.h
@@ -18,9 +18,9 @@ public:
 	{
 		myMenu = new MenuSys(250, 50, "blankmenu.bmp", Auto);
 		myMenu->addButton("button1normal.bmp", "button1down.bmp", "button1over.bmp", CreateFunctionPointer0R(this, &MainMenuState::button1Callback));
-		myMenu->addButton("button2normal.bmp", "button2down.bmp", "button2over.bmp");
-		myMenu->addButton("button3normal.bmp", "button3down.bmp", "button3over.bmp");
-		myMenu->addButton("button4normal.bmp", "button4down.bmp", "button4over.bmp");
+		myMenu->addButton("button2normal.bmp", "button2down.bmp", "button2over.bmp", CreateFunctionPointer0R(this, &MainMenuState::button2Callback));
+		myMenu->addButton("button3normal.bmp", "button3down.bmp", "button3over.bmp", CreateFunctionPointer0R(this, &MainMenuState::button3Callback));
+		myMenu->addButton("button4normal.bmp", "button4down.bmp", "button4over.bmp", CreateFunctionPointer0R(this, &MainMenuState::button4Callback));
 
 		img = new pixmap("statescreens/mainmenu.bmp");
 		img->mY = 618;
@@ -32,6 +32,11 @@ public:
 	void MainMenuState::processMouse(int x, int y);
 	void MainMenuState::processMouseClick(int button, int state, int x, int y);
 	bool button1Callback();
+	bool button2Callback();
+	bool button3Callback();
+	bool button4Callback();
+	bool button5Callback();
+	bool button6Callback();
 
 private:
 
