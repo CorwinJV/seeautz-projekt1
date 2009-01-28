a38ec6bf373816b39fc75e0e84d9c6cd39c44356
corwin.j <corwin.j@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-28 21:39:01 +0000

    State changes...

--- trunk/seeautz/source/glutFramework/glutFramework/States/StartNewGameState.h
+++ trunk/seeautz/source/glutFramework/glutFramework/States/StartNewGameState.h
@@ -2,22 +2,24 @@
 #define STARTNEWGAMESTATE_H
 
 #include ".\..\GameState.h" 
+#include ".\..\pixmap.h"
 
 class StartNewGameState : public GameState
 {
 public:
 	StartNewGameState() {};
-	StartNewGameState(GameStateManager &Parent, int newID) : GameState(Parent, newID)
+	StartNewGameState(GameStateManager &Parent, int newID) : GameState(Parent, newID), img(NULL)
 	{
+		img = new pixmap("statescreens/startnewgame.bmp");
+		img->mY = 618;
 	}
 
 	bool StartNewGameState::Update();
 	bool StartNewGameState::Draw();
 
 private:
-
+	pixmap* img;
 	// add private stuff here
-
 };
 
 #endif
