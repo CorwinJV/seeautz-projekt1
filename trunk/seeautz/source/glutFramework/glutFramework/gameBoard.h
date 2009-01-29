bc1219a94bf98ccd00f4a36f0d140a0d71077b0f
albatross4242@yahoo.com <albatross4242@yahoo.com@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-29 00:17:00 +0000

    more gameboard map stuff, button 4 from the main menu points to the wrong state now, it points to the tutorial map for now

--- trunk/seeautz/source/glutFramework/glutFramework/gameBoard.h
+++ trunk/seeautz/source/glutFramework/glutFramework/gameBoard.h
@@ -13,11 +13,13 @@ class gameBoard
 {
 protected:
 	std::vector<std::vector<mapTile*>> mapList;
+	std::vector<pixmap*> tileImages;
 	// 1 dimensional array of ai entities
 	int Width;
 	int Height;
 
 public:
+	gameBoard();
 	gameBoard(int nwidth, int nheight);
 	~gameBoard();
 	//bool resizeBoard(int x, int y);
@@ -27,6 +29,8 @@ public:
 	bool drawTile(tileTypeEnum ntype, int txPos, int tyPos);
 	bool setTileType(int x, int y, tileTypeEnum ntileType);
 	tileTypeEnum getTileType(int x, int y);	
+	void initialize();
+	void cleanup();
 	// bool addentity
 
 };
