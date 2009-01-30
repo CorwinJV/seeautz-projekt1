8fddc8d443353d5a4a3d53a4e28321963596a42e
albatross4242@yahoo.com <albatross4242@yahoo.com@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-01-30 23:24:06 +0000

    gameboard now should work with pngs

--- seeautz/source/glutFramework/glutFramework/gameBoard.cpp
+++ seeautz/source/glutFramework/glutFramework/gameBoard.cpp
@@ -114,196 +114,69 @@ tileTypeEnum gameBoard::getTileType(int x, int y)
 
 bool gameBoard::drawTile(tileTypeEnum nType, int txPos, int tyPos)
 {
-	pixmap* toDraw;
+	oglTexture2D* toDraw;
 
 	toDraw = tileImages[nType];
-	//switch(nType)
-	//{
-	//case TEmpty:			// 0 empty		
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TDefault:		// 1 default floor tile
-	//	toDraw = tileImages[TDefault];
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TRaised1:		// 2 raised 1 level
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TRaised2:		// 3 raised 2 levels
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TRaised3:		// 4 raised 3 levels
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TRaised4:		// 5 raised 4 levels
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case THalfTopL:		// 6 half top, left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case THalfTopR:		// 7 half top, right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case THalfBottomL:	// 8 half bottom, left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case THalfBottomR:	// 9 half bottom, right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TGap:			// 10 gap
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TElectric:		// 11 electric tile
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TElectricTL:	// 12 electric wall top left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TElectricTR:	// 13 electric wall top right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TElectricBL:	// 14 electric wall bottom left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TElectricBR:	// 15 electric wall bottom right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TIce:			// 16 ice tile
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TWater:			// 17 water tile
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TSwitchTL:		// 18 switch top left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TSwitchTR:		// 19 switch top right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TSwitchBL:		// 20 switch bottom left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TSwitchBR:		// 21 switch bottom right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TSwitch:		// 22 switch whole square
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TProgramTL:		// 23 reprogram spot top left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TProgramTR:		// 24 reprogram spot top right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TProgramBL:		// 25 reprogram spot bottom left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TProgramBR:		// 26 reprogram spot bottom right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TProgram:		// 27 reprogram spot
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TBreakableTL:	// 28 breakable top leftle
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TBreakableTR:	// 29 breakable top right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TBreakableBL:	// 30 breakable bottom left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TBreakableBR:	// 31 breakable bottom right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TSolid:			// 32 Solid Block
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TBreakable:		// 33 Solid Breakable Square
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TStart:			// 34 start square
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TEnd:			// 35 end square
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TDoorTL:		// 36 door top left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TDoorTR:		// 37 door top right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TDoorBL:		// 38 door bottom left
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//case TDoorBR:		// 39 door bottom right
-	//	toDraw = NULL;		// set this to the proper image
-	//	break;
-	//default:
-	//	toDraw = NULL;		// set image to error image
-	//	break;
-	//}
 
 	toDraw->mX = txPos;
 	toDraw->mY = tyPos;
-	toDraw->drawPixMap();
+	toDraw->drawImage();
 
 	return true;
 }
 
 void gameBoard::initialize()
 {
-	//pixmap* temp;
-	//temp = new pixmap("TEmpty.bmp");
+	oglTexture2D* tempTile;
 
-	tileImages.push_back(new pixmap("tiles/TEmpty.bmp"));			
-	tileImages.push_back(new pixmap("tiles/TDefault.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TRaised1.bmp"));
-	tileImages.push_back(new pixmap("tiles/TRaised2.bmp"));
-	tileImages.push_back(new pixmap("tiles/TRaised3.bmp"));
-	tileImages.push_back(new pixmap("tiles/TRaised4.bmp"));
-	tileImages.push_back(new pixmap("tiles/THalfTopL.bmp"));
-	tileImages.push_back(new pixmap("tiles/THalfTopR.bmp"));
-	tileImages.push_back(new pixmap("tiles/THalfBottomL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/THalfBottomR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TGap.bmp"));			
-	tileImages.push_back(new pixmap("tiles/TElectric.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TElectricTL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TElectricTR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TElectricBL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TElectricBR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TIce.bmp"));			
-	tileImages.push_back(new pixmap("tiles/TWater.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TSwitchTL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TSwitchTR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TSwitchBL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TSwitchBR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TSwitch.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TProgramTL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TProgramTR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TProgramBL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TProgramBR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TProgram.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TBreakableTL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TBreakableTR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TBreakableBL.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TBreakableBR.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TSolid.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TBreakable.bmp"));	
-	tileImages.push_back(new pixmap("tiles/TStart.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TEnd.bmp"));			
-	tileImages.push_back(new pixmap("tiles/TDoorTL.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TDoorTR.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TDoorBL.bmp"));		
-	tileImages.push_back(new pixmap("tiles/TDoorBR.bmp"));			
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TEmpty.png",		 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TEmpty.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TDefault.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TRaised1.png",	 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TRaised2.png",	 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TRaised3.png",	 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TRaised4.png",	 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/THalfTopL.png",	 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/THalfTopR.png",	 144, 72); tileImages.push_back(tempTile);
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/THalfBottomL.png", 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/THalfBottomR.png", 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TGap.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TElectric.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TElectricTL.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TElectricTR.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TElectricBL.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TElectricBR.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TIce.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TWater.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TSwitchTL.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TSwitchTR.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TSwitchBL.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TSwitchBR.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TSwitch.png",		 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TProgramTL.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TProgramTR.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TProgramBL.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TProgramBR.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TProgram.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TBreakableTL.png", 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TBreakableTR.png", 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TBreakableBL.png", 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TBreakableBR.png", 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TSolid.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TBreakable.png",	 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TStart.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TEnd.png",		 144, 72); tileImages.push_back(tempTile);		
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TDoorTL.png",		 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TDoorTR.png",		 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TDoorBL.png",		 144, 72); tileImages.push_back(tempTile);	
+	tempTile = new oglTexture2D(); tempTile->loadImage("tiles/TDoorBR.png",		 144, 72); tileImages.push_back(tempTile);			
 
 
 }
 
 void gameBoard::cleanup()
 {
-	vector<pixmap*>::iterator itr = tileImages.begin();
+	vector<oglTexture2D*>::iterator itr = tileImages.begin();
 	for(; itr != tileImages.end(); itr++)
 	{
 		delete (*itr);
