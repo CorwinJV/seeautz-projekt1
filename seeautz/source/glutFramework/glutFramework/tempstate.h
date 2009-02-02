2c042f88467d7e1253180e8fd731621d535842f2
albatross4242@yahoo.com <albatross4242@yahoo.com@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-02-02 20:33:30 +0000

    oh yeah and these

---
+++ seeautz/source/glutFramework/glutFramework/tempstate.h
@@ -0,0 +1,38 @@
+#ifndef TEMPSTATE_H
+#define TEMPSTATE_H
+
+#include "GameState.h" 
+#include "oglUtility.h"
+#include "pixmap.h"
+#include "oglTexture2D.h"
+
+
+class tempstate : public GameState
+{
+private:
+	oglTexture2D* logo;
+	int timer;
+
+
+public:
+
+	tempstate() {};
+	tempstate(GameStateManager &Parent, int newID) : GameState(Parent, newID), logo(NULL)
+	{
+		logo = new oglTexture2D();
+		if(logo != NULL)
+			logo->loadImage("button4over.png", 1024, 768);
+
+		//logo->mX = -500;
+		//logo->mY = 500;
+		timer = 0;
+	}
+
+	bool tempstate::Update();
+	bool tempstate::Draw();
+
+};
+
+
+#endif
+
