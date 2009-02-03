b529844f75e03b4fa59ba47d08b46a12bea6ad5e
tomlindeman@comcast.net <tomlindeman@comcast.net@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-02-03 00:01:31 +0000

    Menu now works in the ProfileMgrState, suck it Corwin!

--- seeautz/source/glutFramework/glutFramework/States/ProfileMgrState.h
+++ seeautz/source/glutFramework/glutFramework/States/ProfileMgrState.h
@@ -2,7 +2,9 @@
 #define PROFILEMGRSTATE_H
 
 #include ".\..\GameState.h" 
+#include ".\..\GameStateManager.h"
 #include ".\..\oglTexture2D.h"
+#include "MainMenuState.h"
 
 class ProfileMgrState : public GameState
 {
@@ -14,12 +16,17 @@ public:
 		if(img != NULL)
 			img->loadImage("..\\Content\\statescreens\\profilemanagement.png", 1024, 120);
 		img->mY = 618;
+
+		myMenu = new MenuSys(250, 50, "blankmenu.png", Auto);
+		myMenu->addButton("..\\Content\\buttons\\returntomainmenu.png", "button1down.png", "button1over.png", CreateFunctionPointer0R(this, &ProfileMgrState::MainMenuStateCallback));
+		Update();
 	}
 
 	void processMouse(int x, int y);
 	void processMouseClick(int button, int state, int x, int y);
 	bool ProfileMgrState::Update();
 	bool ProfileMgrState::Draw();
+	bool MainMenuStateCallback();
 
 private:
 	oglTexture2D* img;
