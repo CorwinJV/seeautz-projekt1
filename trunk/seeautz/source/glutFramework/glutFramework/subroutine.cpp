a3d9e38a2d6baa0c3a048fd9c5da3ab5d99fd2f7
albatross4242@yahoo.com <albatross4242@yahoo.com@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-02-26 05:29:09 +0000

    oh yeah and here's the code

---
+++ trunk/seeautz/source/glutFramework/glutFramework/subroutine.cpp
@@ -0,0 +1,73 @@
+#include "subroutine.h"
+
+subroutine::subroutine()
+{
+	currentInstruction = instructionList.begin();
+	objectType = OSubroutine;
+}
+
+subroutine::~subroutine()
+{
+}
+
+bool subroutine::advanceCommand()
+{
+	currentInstruction++;
+	if(currentInstruction == instructionList.end())
+	{
+		currentInstruction = instructionList.begin();
+		return true;
+	}
+	return false;
+}
+
+AiInstructions subroutine::getNextCommand()
+{
+	return (*currentInstruction)->enumInstruction;
+}
+
+void subroutine::draw()
+{
+}
+
+void subroutine::update()
+{
+}
+
+objectEnum subroutine::getType()
+{
+	return objectType;
+}
+
+void subroutine::addCommand(logicBlock* newCommand)
+{
+	instructionList.push_back(newCommand);
+	currentInstruction = instructionList.begin();
+}
+
+void subroutine::removeLastCommand()
+{
+	instructionList.pop_back();
+}
+
+void subroutine::clearInstructions()
+{
+	instructionList.clear();
+}
+
+void subroutine::coreDump()
+{
+	vector <logicBlock*>::iterator itr = instructionList.begin();
+
+	std::cout << "Subroutine Core Dump: " << endl;
+	for(;itr < instructionList.end(); itr++)
+	{
+		std::cout << (*itr)->enumInstruction << " ";
+	}
+	std::cout << endl;
+}
+
+bool subroutine::isEmpty()
+{
+	return instructionList.empty();
+}
