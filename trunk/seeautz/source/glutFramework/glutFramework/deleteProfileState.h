5041a1b683d93fcccabfebc8c4562c6b5edc2eb7
corwin.j <corwin.j@6d558e22-e0db-11dd-bce9-bf72c0239d3a>
2009-04-08 02:55:40 +0000

    Delete profile state now has the same look and feel of select profile state, your welcome Dave...

--- trunk/seeautz/source/glutFramework/glutFramework/deleteProfileState.h
+++ trunk/seeautz/source/glutFramework/glutFramework/deleteProfileState.h
@@ -1,11 +1,12 @@
-#ifndef DELETEPROFILESTATE_H
-#define DELETEPROFILESTATE_H
+#ifndef deletePROFILESTATE_H
+#define deletePROFILESTATE_H
 
 #include "GameState.h" 
 #include "GameStateManager.h"
 #include "oglTexture2D.h"
 #include "playGame.h"
 #include "clickOKState.h"
+#include "oglGameVars.h"
 
 
 class deleteProfileState : public GameState
@@ -14,25 +15,49 @@ public:
 	deleteProfileState() {};
 	deleteProfileState(GameStateManager &Parent, int newID) : GameState(Parent, newID)
 	{
-		deletingProfile = true;
-		checked = 0;
-		myMenu = new MenuSys(250, 250, "blankmenu.png", Auto);
+		done = 0;
+		maxNumProfiles = GameVars->PM->getMaxRecords() - 1;
+		profileViewing = 0;
+		if(maxNumProfiles >= 0)
+		{
+			GameVars->PM->setCurrentRecord(profileViewing);
+		}
+	//	myMenu = new MenuSys(250, 250, "blankmenu.png", Auto);
+		myMenu = new MenuSys(250, 50, "blank.png", None);
+		myMenu->addButton("arrow_left.png", "arrow_lefthover.png", "arrow_lefthover.png", CreateFunctionPointer0R(this, &deleteProfileState::decrement));
+		myMenu->setLastButtonDimensions(100, 100);
+		myMenu->setLastButtonPosition(150, 600);
+		myMenu->addButton("arrow_right.png", "arrow_righthover.png", "arrow_righthover.png", CreateFunctionPointer0R(this, &deleteProfileState::increment));
+		myMenu->setLastButtonDimensions(100, 100);
+		myMenu->setLastButtonPosition(775, 600);
+		myMenu->addButton("buttons\\deleteprofile.png", "buttons\\deleteprofilehover.png", "buttons\\deleteprofilehover.png", CreateFunctionPointer0R(this, &deleteProfileState::deleteProfile));
+		myMenu->setLastButtonDimensions(475, 100);
+		myMenu->setLastButtonPosition(275, 600);
+		myMenu->addButton("buttons\\back.png", "buttons\\backhover.png", "buttons\\backhover.png", CreateFunctionPointer0R(this, &deleteProfileState::back));
+		myMenu->setLastButtonDimensions(100, 50);
+		myMenu->setLastButtonPosition(750, 250);
+		
 		Update();
 	}
 
 	void processMouse(int x, int y);
 	void processMouseClick(int button, int state, int x, int y);
-	void deleteProfileState::keyboardInput(unsigned char c, int x, int y);
+	void keyboardInput(unsigned char c, int x, int y);
 	bool deleteProfileState::Update();
 	bool deleteProfileState::Draw();
-	bool deleteProfileState::doesNameAlreadyExists(std::string);
-	void deleteProfileState::setPlayerInfo(std::string name, int, int, int);
+	bool increment();
+	bool decrement();
+	bool back();
+	bool deleteProfile();
 	
-	int checked;
-	std::string tempString; 
-
+	
 private:
-	bool deletingProfile;
+	int profileViewing;
+	int maxNumProfiles;
+	int totScore;
+	int highestLevel;
+	std::string playerName; 
+	int done;
 	// add private stuff here
 
 };
